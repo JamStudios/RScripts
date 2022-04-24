1265f9e206fc2c0d5fcdb4369510ea238d79d909
Jam The Adventurer <94430800+JamTheAdventurer@users.noreply.github.com>
2022-04-24 13:30:19 +0800

    Update Loadstring.lua

--- Scripts/TowerOfHell/VG Hub/Loadstring.lua
+++ Scripts/TowerOfHell/VG Hub/Loadstring.lua
@@ -1,1 +1,10 @@
+-- Information:
+-- Updated since: April 24th 2022
+-- Status: Working
+--
+--  NOTE: If This Script no longer works please make an issue regarding the script no longer working.
+
+loadstring(game:HttpGet(("https://pastebin.com/raw/dJeV6Via"),true))()
+-- FPS Booster ^^
 loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
+-- VG Hub ^^
