92e7e11ae63dab3ae86e8477442684ffa341a644
Jam The Adventurer <94430800+JamTheAdventurer@users.noreply.github.com>
2022-04-24 13:29:01 +0800

    Update Loadstring.lua

--- Scripts/Bloxfruits/MokuroHUB+/Loadstring.lua
+++ Scripts/Bloxfruits/MokuroHUB+/Loadstring.lua
@@ -1,12 +1,12 @@
+-- Information:
+-- Updated since: April 6th 2022
+-- Status: Working
+--
+--  NOTE: If This Script no longer works please make an issue regarding the script no longer working.
+
 loadstring(game:HttpGet(("https://pastebin.com/raw/dJeV6Via"),true))()
 -- FPS Booster ^^
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xDepressionx/Free-Script/main/BloxFruit.lua"))()
 -- Make a "--" if your using a bloxfruit that has a sword ^^
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/koonpeatch/PeatEX/master/BKHAX/BloxFruits"),true))()
 -- Main Script MokuroHUB x BKHAX ^^
-
--- Information:
--- Updated since: April 6th
--- Status: Working
---
--- If This Script no longer works please make an issue regarding the script no longer working.
